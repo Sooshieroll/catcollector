1fa18cefa4f286be5423e0f98a0fef0b84420b4e
Sooshieroll <imlucywang@gmail.com>
2022-11-29 21:39:32 -0800

    feat: add views and urls to main_app

---
+++ main_app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse('Hello World! 😇')
+
+
+def about(request):
+    return HttpResponse('About Page')
+
+
+def contact(request):
+    return HttpResponse('Contact Page')
+
+
+def blog(request):
+    return HttpResponse('Blog Page')
