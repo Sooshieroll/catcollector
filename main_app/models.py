17185514ba166608e73186ca45baf97fcdc4156d
Sooshieroll <imlucywang@gmail.com>
2022-12-06 21:25:59 -0800

    feat: add profile page

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,9 +9,11 @@ class Cat(models.Model):
     breed = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     age = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
 
 class Dog(models.Model):
     name = models.CharField(max_length=100)
@@ -20,4 +23,3 @@ class Dog(models.Model):
 
     def __str__(self):
         return self.name
-
--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,9 @@ from .models import Cat, Dog
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 
 class DogCreate(CreateView):
@@ -16,6 +19,12 @@ class CatCreate(CreateView):
     model = Cat
     fields = '__all__'
     success_url = '/cats'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect('/cats')
 
 
 class CatUpdate(UpdateView):
@@ -80,3 +89,31 @@ def cats_show(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
 
     return render(request, 'cats/show.html', {'cat': cat})
+
+
+def profile(request, username):
+    user = User.objects.get(username=username)
+    cats = list(Cat.objects.filter(user=user))
+
+    return render(request, 'profile.html', {'username': username, 'cats': cats})
+
+
+def login_view(request):
+    # if post, then authenticate (user submitted username and password)
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username=u, password=p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/user/'+u)
+                else:
+                    print('The account has been disabled.')
+            else:
+                print('The username and/or password is incorrect.')
+    else:  # it was a get request so send the emtpy login form
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
